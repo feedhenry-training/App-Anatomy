52d615e73ddce8cd12083b89f32980ff58189f23
David Martin <david.martin@feedhenry.com>
2012-02-01 12:20:37 +0000

    Migrate server side to node.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,15 @@
-function getConfig() {
-  return {data:config};
-}
+var util = require('util');
+/* main.js
+ * All calls here are publicly exposed as REST API endpoints.
+ * - all parameters must be passed in a single JSON paramater.
+ * - the return 'callback' method signature is 'callback (error, data)', where 'data' is a JSON object.
+*/
+
+/* 'getConfig' server side REST API method.
+ * Trivial example of pulling in a shared config file.
+ */
+exports.getConfig = function(params, callback) {
+  console.log('in getConfig');
+  var cfg = require("config.js");
+  return callback(null, {data: cfg.config});
+};
